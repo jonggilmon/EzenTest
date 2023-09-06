05f919120ad86f0c49c830255e5197a3c85416d9
jonggil <jonggil0309@gmail.com>
2023-09-06 21:52:05 +0900

    초기

---
+++ demoz3223/src/main/java/com/example/demo/Demoz3223Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoz3223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoz3223Application.class, args);
+	}
+
+}
